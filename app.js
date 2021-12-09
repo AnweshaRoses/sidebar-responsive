10bc16d1d6b81b034bb3f48c000e0b32ab60a5b3
Anwesha Mishra <anwesharoses@gmail.com>
2021-12-10 00:02:01 +0800

    another way for color changing navbar

--- app.js
+++ app.js
@@ -2,25 +2,32 @@ const dark=document.querySelector(".dark")
 const night=document.querySelector(".night")
 const light=document.querySelector(".light")
 const sidebar=document.querySelector(".sidebar");
-const activeList=document.querySelector(".left-menu-icon.active");
-const color=document.querySelector(".color-box.light");
+const activeList=document.querySelector(".left-menu-icon");
+const color=document.querySelector("#box3");
+const color1=document.querySelector("#box1");
+const color2=document.querySelector("#box2");
 
 
 dark.addEventListener("click", ()=>{
     sidebar.className= "sidebar dark";
-
-
+    color.className="color-box light sammy1";
+    color1.className="color-box dark sammy1";
+    color2.className="color-box night sammy1";
+    
 })
 night.addEventListener("click", ()=>{
     sidebar.className= "sidebar night";
-
-
-
+    color.className="color-box light sammy1";
+    color1.className="color-box dark sammy1";
+    color2.className="color-box night sammy1";
 
 })
 light.addEventListener("click", ()=>{
     sidebar.className= "sidebar light";
-    activeList.className += " "+"left-menu-icon"+ " "+"active"+ " "+"light";
-    color.className="color-box light sammy";
+    activeList.className = "left-menu-icon active light"
+    color.className="color-box light sammy2";
+    color1.className="color-box dark sammy2";
+    color2.className="color-box night sammy2";
+    
 
 })
